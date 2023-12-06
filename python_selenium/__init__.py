15ec3cb83bb1b6145dc4a5189b6e5684783dc6b0
Pawan Rai <pawanr@LT-MBP-031.local>
2023-12-06 09:44:49 +0530

    ✨python selenium sdk

---
+++ python_selenium/__init__.py
@@ -0,0 +1,2 @@
+from python_selenium.version import __version__
+from python_selenium.smartui import smartui_snapshot
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import python_selenium
+
+setup(
+    name="@lambdatest/python-selenium-sdk",
+    version=python_selenium.__version__,
+    author="LambdaTest <keys@lambdatest.com>",
+    description="Python Selenium SDK for testing with Smart UI",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="git+https://github.com/LambdaTest/lambdatet-sdk",
+    keywords="lambdatest python selenium sdk",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "requests",
+        "selenium"
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable"
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
