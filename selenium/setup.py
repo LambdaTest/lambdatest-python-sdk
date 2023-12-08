a951c07d82a2de7493a6b5a0dd21bc1e158a3b62
Pawan Rai <pawanr@LT-MBP-031.local>
2023-12-08 17:43:59 +0530

    added readme path in setup

--- selenium/setup.py
+++ selenium/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 
 cwd = path.abspath(path.dirname(__file__))
-with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+with open(path.join(cwd, 'readme.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
