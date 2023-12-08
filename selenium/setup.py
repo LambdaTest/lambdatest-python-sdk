7e1426ba5cdd9e61ae04d44b1ee639552f87daa2
Pawan Rai <pawanr@LT-MBP-031.local>
2023-12-08 17:40:39 +0530

    fixed workflow

--- selenium/setup.py
+++ selenium/setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+from os import path
+
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="lambdatest-selenium-driver",
